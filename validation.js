ea97a13210e9ff6f7e4d4df613f05a2a5a745f5b
Habeeb <habeebav841@gmail.com>
2022-09-04 11:48:38 -0700

    hai

--- validation.js
+++ validation.js
@@ -1,54 +1,62 @@
-$(document).ready(function(){
-    $("#submit-form").validate({
+// $(document).ready(function(){
+//     $("#submit-form").validate({
 
-      rules:{
-        Name:{
-            required:true,
-            minlength:4,
-			maxlength:20
-        },
-        Email:{
-            required:true,
-            minlength:4,
-			email:true
-        },
-        Phone:{
-          required:true,
-          minlength:10,
-		  maxlength:10
-        },
-		Message:{
-			required:true,
-			maxlength:100
-		}
-      },
-      messages:{
-		Name:{
-        required:"required",
-		minlength:"enterpropername",
-		maxlength:"exceedlimit"
-	},
-	Email:{
-		required:"required",
-		minlength:"enterproperEmail",
-		email:"NotEmail"
+//       rules:{
+//         Name:{
+//             required:true,
+//             minlength:4,
+// 			maxlength:20
+//         },
+//         Email:{
+//             required:true,
+//             minlength:4,
+// 			email:true
+//         },
+//         Phone:{
+//           required:true,
+//           minlength:10,
+// 		  maxlength:10
+//         },
+// 		Message:{
+// 			required:true,
+// 			maxlength:100
+// 		}
+//       },
+//       messages:{
+// 		Name:{
+//         required:"required",
+// 		minlength:"enterpropername",
+// 		maxlength:"exceedlimit"
+// 	},
+// 	Email:{
+// 		required:"required",
+// 		minlength:"enterproperEmail",
+// 		email:"NotEmail"
 
-	},
-	Phone:{
-		required:"required",
-		digits:true,
-		minlength:"notvalid",
-		maxlength:"notvalid"
+// 	},
+// 	Phone:{
+// 		required:"required",
+// 		digits:true,
+// 		minlength:"notvalid",
+// 		maxlength:"notvalid"
 		
-	},
-	Message:{
-		required:"required",
-		maxlength:"Exceeded"
+// 	},
+// 	Message:{
+// 		required:"required",
+// 		maxlength:"Exceeded"
 
-	}
-      }
-    })
-})
+// 	}
+//       }
+//     })
+// })
+
+
+
+
+
+
+
+
 // $(document).ready(function(){
 //   $("#submit-form").validate({
 
@@ -180,3 +188,66 @@ $(document).ready(function(){
 
 // })
 	
+
+
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var subjectError = document.getElementById('subject-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+
+function validateName() {
+    var name=document.getElementById('contact-name').value.trim();  
+
+    if (name.length==0) {
+        nameError.innerHTML='Name is Required';
+        nameError.style.color='red'
+        return false;
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML='Write a FullName';
+        nameError.style.color='red'
+        return false;
+    }
+    
+    nameError.innerHTML='Name is valid';
+    nameError.style.color='green'
+    return true;
+}
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value.trim();
+    if (email.length==0) {
+        emailError.innerHTML='Email is Required';
+        emailError.style.color='red'
+        return false;
+    }
+
+    if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailError.innerHTML='Email is Invalid';
+        emailError.style.color='red'
+        return false;
+    }
+    emailError.innerHTML='Email is valid';
+    emailError.style.color='green'
+    return true;
+}
+
+function validateMessage() {
+    var message =document.getElementById('contact-message').value.trim();
+    var Required=15;
+    var left=Required - message.length;
+    
+    if(left>0){
+        messageError.innerHTML =left+ 'more character Required';
+        messageError.style.color='red'
+        return false;
+    
+    }
+    messageError.innerHTML='Message is valid';
+    messageError.style.color='green'
+    return true;    
+    
+}
