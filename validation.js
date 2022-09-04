3226524931294a1f3d2b3a3bc6facb3b0dd80b63
Habeeb <habeebav841@gmail.com>
2022-09-04 14:35:48 -0700

    final

--- validation.js
+++ validation.js
@@ -12,6 +12,8 @@ function validateName() {
 	if (name.length == 0) {
 		nameError.innerHTML = 'Name is Required';
 		nameError.style.color = 'red'
+		document.getElementById("contact-name").style.borderColor = "red"
+		document.getElementById("contact-name").style.color = "red"
 
 		return false;
 	}
@@ -19,6 +21,8 @@ function validateName() {
 	if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
 		nameError.innerHTML = 'Write a FullName';
 		nameError.style.color = 'red'
+		document.getElementById("contact-name").style.borderColor = "red"
+		document.getElementById("contact-name").style.color = "red"
 		return false;
 	}
 
@@ -43,6 +47,8 @@ function validateEmail() {
 	if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
 		emailError.innerHTML = 'Email is Invalid';
 		emailError.style.color = 'red'
+		document.getElementById("contact-email").style.borderColor = "red"
+		document.getElementById("contact-email").style.color = "red"
 		return false;
 	}
 	emailError.innerHTML = '';
@@ -56,20 +62,23 @@ function validatePhone() {
 	if (phone.length == 0) {
 		phoneError.innerHTML = 'phone number is Required';
 		phoneError.style.color = 'red'
+		document.getElementById("contact-phone").style.borderColor = "red"
+		document.getElementById("contact-phone").style.color = "red"
 		return false;
 	}
 	if (phone.length != 10) {
 		phoneError.innerHTML = "Enter Valid phonenumber"
 		phoneError.style.color = "red"
-
-
-
+		document.getElementById("contact-phone").style.borderColor = "red"
+		document.getElementById("contact-phone").style.color = "red"
 		return false
 
 	}
 	if (!phone.match(/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/)) {
 		phoneError.innerHTML = "Phonenumber must be digits"
 		phoneError.style.color("red")
+		document.getElementById("contact-phone").style.borderColor = "red"
+		document.getElementById("contact-phone").style.color = "red"
 
 
 		return false;
@@ -91,6 +100,8 @@ function validateMessage() {
 	if (left > 0) {
 		messageError.innerHTML = left + 'more character Required';
 		messageError.style.color = 'red'
+		document.getElementById("contact-message").style.borderColor = "red"
+		document.getElementById("contact-message").style.color = "red"
 		return false;
 
 	}
